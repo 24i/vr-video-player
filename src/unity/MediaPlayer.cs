56486aa5cc0bbe9eca90c23c5ab07890e3ed0b48
Bas Tuijnman <bas.tuijnman@24i.com>
2017-01-13 13:48:34 +0100

    Fix enum index in behaviour

--- src/unity/MediaPlayer.cs
+++ src/unity/MediaPlayer.cs
@@ -5,44 +5,47 @@ using System;
 using System.IO;
 
 public enum MediaPlayerState {
-    STOPPED,
-    PAUSED,
-    PLAYING,
-    BUFFERING
+	STOPPED,
+	PAUSED,
+	PLAYING,
+	BUFFERING
 }
 
 public class MediaPlayer : MonoBehaviour {
 
 	#region Plugin imports
-    [DllImport("main")]
-    private static extern IntPtr GetRenderEventFunc();
+	[DllImport("main")]
+	private static extern IntPtr GetRenderEventFunc();
 
-    [DllImport("main")]
-    private static extern void SetDebugCallback (IntPtr ptrFunc);
+	[DllImport("main")]
+	private static extern void SetDebugCallback (IntPtr ptrFunc);
 
-    [DllImport("main")]
-    private static extern void SetAudioCallback (IntPtr ptrFunc);
+	[DllImport("main")]
+	private static extern void SetAudioCallback (IntPtr ptrFunc);
 
-    [DllImport("main")]
-    private static extern void SetUnityTexture (IntPtr tex);
+	[DllImport("main")]
+	private static extern void SetUnityTexture (IntPtr tex);
 
-    [DllImport("main")]
-    private static extern void VRVideoPlayerSetup (string file);
+	[DllImport("main")]
+	private static extern void VRVideoPlayerSetup (string file);
 
-    [DllImport("main")]
-    private static extern void VRVideoPlayerDestroy ();
+	[DllImport("main")]
+	private static extern void VRVideoPlayerDestroy ();
 
-    [DllImport("main")]
-    private static extern void VRVideoPlayerPlay();
+	[DllImport("main")]
+	private static extern void VRVideoPlayerPlay();
 
-    [DllImport("main")]
-    private static extern void VRVideoPlayerPause();
+	[DllImport("main")]
+	private static extern void VRVideoPlayerPause();
 
-    [DllImport("main")]
-    private static extern void VRVideoPlayerStop();
+	[DllImport("main")]
+	private static extern void VRVideoPlayerStop();
 
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void DebugDelegate (string str);
+	[DllImport("main")]
+	private static extern int VRVideoPlayerGetState();
+
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	public delegate void DebugDelegate (string str);
 	#endregion
 
 	#region Media player properties
@@ -50,47 +53,48 @@ public class MediaPlayer : MonoBehaviour {
 	public int width;
 	public int height;
 
-    private Texture2D videoTexture;
+	private Texture2D videoTexture;
 	#endregion
 
 	#region Behaviour callbacks
-    IEnumerator Start () {
+	IEnumerator Start () {
 
-        // Setup debug
-        DebugDelegate debugDelegate = new DebugDelegate(DebugCallback);
-        IntPtr ptrIntDelegate = Marshal.GetFunctionPointerForDelegate (debugDelegate);
-        SetDebugCallback (ptrIntDelegate);
+		// Setup debug
+		DebugDelegate debugDelegate = new DebugDelegate(DebugCallback);
+		IntPtr ptrIntDelegate = Marshal.GetFunctionPointerForDelegate (debugDelegate);
+		SetDebugCallback (ptrIntDelegate);
 
-        VRVideoPlayerSetup (mediaFile);
+		VRVideoPlayerSetup (mediaFile);
 
-        // Setup video texture
+		// Setup video texture
 		videoTexture = new Texture2D (width, height, TextureFormat.RGB24, false);
 		videoTexture.Apply();
 		GetComponent<Renderer> ().material.mainTexture = videoTexture;
 
-        // Attach unity texture to plugin
+		// Attach unity texture to plugin
 		SetUnityTexture (videoTexture.GetNativeTexturePtr ());
+		Debug.Log (GetState ());
 
-        yield return StartCoroutine("CallPluginAtEndOfFrames");
-    }
+		yield return StartCoroutine("CallPluginAtEndOfFrames");
+	}
 
-    void OnDestroy () {
-        VRVideoPlayerStop ();
-    }
+	void OnDestroy () {
+		VRVideoPlayerStop ();
+	}
 
 	static void DebugCallback (string str) {
 		Debug.Log ("Plugin: " + str);
 	}
 
-    private IEnumerator CallPluginAtEndOfFrames()
-    {
-        while (true) {
-            // Wait until all frame rendering is done
-            yield return new WaitForEndOfFrame();
+	private IEnumerator CallPluginAtEndOfFrames()
+	{
+		while (true) {
+			// Wait until all frame rendering is done
+			yield return new WaitForEndOfFrame();
 
-            GL.IssuePluginEvent(GetRenderEventFunc(), 1);
-        }
-    }
+			GL.IssuePluginEvent(GetRenderEventFunc(), 1);
+		}
+	}
 	#endregion
 
 	#region Media player public API
@@ -98,24 +102,29 @@ public class MediaPlayer : MonoBehaviour {
 	/// <summary>
 	/// Starts media player playback
 	/// </summary>
-    public void Play () {
-        VRVideoPlayerPlay ();
-    }
+	public void Play () {
+		VRVideoPlayerPlay ();
+	}
 
 	/// <summary>
 	/// Pauses the media player
 	/// </summary>
-    public void Pause () {
-        VRVideoPlayerPause ();
-    }
+	public void Pause () {
+		VRVideoPlayerPause ();
+	}
 
 	/// <summary>
 	/// Get the media player state
 	/// </summary>
 	/// <returns>The media player state.</returns>
-    public int GetState () {
-        return (MediaPlayerState)VRVideoPlayerGetState();
-    }
+	public MediaPlayerState GetState () {
+
+		/*
+		 * Plugin enums are based on natural indexes, convert to 
+		 * 0 based index to use with behavior enum
+		 */
+		return (MediaPlayerState)(VRVideoPlayerGetState() - 1);
+	}
 
 	public void SetPosition () {
 		Debug.Log ("Method not yet implemented");
